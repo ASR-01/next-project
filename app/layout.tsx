c4d183c02bddaf472ea2db793ba45a43ae18a4da
ASR-01 <gamingasr002@gmail.com>
2024-01-15 15:55:45 +0530

    Added Assetes folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,31 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description:
+    "An community driven Application for asking and and answering programming Questions",
+
+  icons: {
+    icon: "/public/assets/images/site-logo.svg",
+  },
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export default function RootLayout({
   children,
@@ -8,9 +33,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <head>
+          <link rel="icon" href="/favicon.ico" />
+        </head>
+        <body
+          className={`${inter.variable} ${spaceGrotesk.variable}`}
+          suppressHydrationWarning={true}
+        >
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
